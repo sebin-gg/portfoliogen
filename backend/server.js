4521d51156fcb44e51ea4d170f3a9af78ed0aa44
Sebin Mathew <sebinmathew543@gmail.com>
2025-06-09 19:47:52 +0530

    changed names of themes in folder

--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,7 @@ app.use(express.json());
 
 app.use('/public',express.static(path.join(__dirname,'public')));
 app.post('/form', (req, res) => {
-  const data = req.body;        // Get JSON data from request body
-  const theme = data.theme;     
+  const { theme, ...data } = req.body
   // Define directory path to save user data
 const templatepath=path.join(__dirname,'templates',`${theme}.html`);
 const outputDir = path.join(__dirname, 'public');
@@ -20,11 +19,12 @@ const outputPath = path.join(outputDir,outputFileName);
   fs.readFile(templatepath, 'utf-8', (err, html) => {
     if (err) return res.status(500).send('Template not found.');
 
-    const filledHtml = html.replace('__DATA__', JSON.stringify(data-theme));
+    const filledHtml = html.replace('__DATA__', JSON.stringify(data));
 
     fs.writeFile(outputPath, filledHtml, (err) => {
       if (err) return res.status(500).send('Failed to write HTML file.');
       res.send({ link: `/public/${outputFileName}` });
+      res.download(filledHtml)
     });
   });
 });
