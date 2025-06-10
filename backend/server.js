0d50605c6d1d3da2b7d2ee211735956351351f69
Sebin Mathew <69630250+sebin-gg@users.noreply.github.com>
2025-06-10 10:38:33 +0530

    removed extenal css files and inserteed the styles into eaxh html file

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const path = require('path');
 app.use(cors());
 app.use(express.json());
 
-// Serve static files
+
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.post('/form', (req, res) => {
