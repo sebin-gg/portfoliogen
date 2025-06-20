32af3f76d2bc516d3d805a706b1fba3a12bda472
Sebin Mathew <sebinmathew543@gmail.com>
2025-06-20 20:35:37 +0530

    tried to add image support

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,18 @@
 const express = require('express');
-const cors = require('cors');
+const multer = require('multer');
 const app = express();
+const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
-
-app.use(cors());
-app.use(express.json());
-
+const upload = multer();
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
-app.post('/form', (req, res) => {
-  const { theme, ...data } = req.body; 
-  console.log('Theme received:', theme);
+app.post('/form', upload.none(), (req, res) => {
+  console.log(req.body);
+  const { theme,profilePicture,...data } = req.body; 
+  console.log(data);
+  if (!data) {return res.status(400).send("Data is missing.")};
   if (!theme) {return res.status(400).send("Theme is missing.")};
   const templatePath = path.join(__dirname, 'templates', `${theme}.html`);
   const outputDir = path.join(__dirname, 'public');
@@ -23,10 +23,13 @@ app.post('/form', (req, res) => {
   fs.readFile(templatePath, 'utf-8', (err, html) => {
     if (err) return res.status(500).send('Template not found',err);
 
-    const filledHtml = html.replace('__DATA__', JSON.stringify(data).replace(/</g, '\\u003c'));
+    const filledHtml = html
+    .replace('__DATA__', JSON.stringify(data).replace(/</g, '\\u003c'))
+    .replace('__IMG__', profilePicture);
 
     fs.writeFile(outputPath, filledHtml, (err) => {
       if (err) return res.status(500).send('Failed to write HTML file.');
+      
 
       const filePath = path.join(__dirname, 'public', outputFileName);
       res.download(filePath); // Force download
