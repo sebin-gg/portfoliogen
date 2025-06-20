41648446f649e1dedeea5eba706da3bd7a09e788
Sebin Mathew <69630250+sebin-gg@users.noreply.github.com>
2025-06-20 21:50:47 +0530

    image support added but things are weird now

--- frontend/src/pages/InputPage.jsx
+++ frontend/src/pages/InputPage.jsx
@@ -45,44 +45,39 @@ const InputPage = () => {
  
   const handleSubmit = (event) => {
     event.preventDefault();
-
+    
     const form = event.target;
+const formData = new FormData();
+formData.append("fullName", form.fullName.value);
+formData.append("aboutMe", form.aboutMe.value);
+formData.append("email", form.email.value);
+formData.append("phone", form.phone.value);
+formData.append("theme", form.theme.value);
+formData.append("profilePicture", form.profilePicture.files[0]);
 
-    const formData = {
-      fullName: form.fullName.value,
-      aboutMe: form.aboutMe.value,
-      email: form.email.value,
-      phone: form.phone.value,
-      skills,
-      projects,
-      socialLinks: {
-        linkedin: form.linkedin.value,
-        github: form.github.value,
-        instagram: form.instagram.value,
-      },
-      theme: form.theme.value,
-      profilePicture:form.profilePicture.file,
-    };
+formData.append("skills", JSON.stringify(skills));
+formData.append("projects", JSON.stringify(projects));
+formData.append("socialLinks", JSON.stringify({
+  linkedin: form.linkedin.value,
+  github: form.github.value,
+  instagram: form.instagram.value,
+}));
 
-    // POST data as JSON
-    fetch("http://localhost:3001/form", {
-      method: "POST",
-      body: formData,
-
-    })
-      .then((res) => res.blob())
-      .then((blob) => {
-        const url = window.URL.createObjectURL(blob);
-        const a = document.createElement("a");
-        a.href = url;
-        a.download = "portfolio.html";
-        a.click();
-        navigate("/result");
-        console.log('client json sent');
-      })
-      .catch((err) => console.log('json not sent',err));
-  };
-
+fetch("http://localhost:3001/form", {
+  method: "POST",
+  body: formData,
+}).then((res) => res.blob())
+  .then((blob) => {
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "portfolio.html";
+    a.click();
+    navigate("/result");
+    console.log("client json sent");
+  })
+  .catch((err) => console.log("json not sent", err));
+}
   return (
     <form className="input-page" onSubmit={handleSubmit}>
       <div className="input-content">
