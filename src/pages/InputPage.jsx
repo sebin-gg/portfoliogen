f36cd2ac363e6120e54899e39c3951d21e48b716
Sebin Mathew <69630250+sebin-gg@users.noreply.github.com>
2025-07-12 11:33:23 +0530

    none

--- src/pages/InputPage.jsx
+++ src/pages/InputPage.jsx
@@ -1,8 +1,7 @@
 import  { useState } from "react";
 import "../styles/InputPage.css";
 import { useNavigate } from "react-router-dom";
-const API_URL = import.meta.env.VITE_API_URL;
-
+import "../../frontend/api";
 
 const InputPage = () => {
 
